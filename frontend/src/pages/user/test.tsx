dd6d215517458e77500ed54485d5226b696db8e4
darmiel <71837281+darmiel@users.noreply.github.com>
2023-12-31 15:12:53 +0100

    chore[frontend]: removed test page

--- frontend/src/pages/user/test.tsx
+++
@@ -1,9 +0,0 @@
-import CreateMeetingModal from "@/components/meeting/modals/CreateMeetingModal"
-
-export default function TestPage() {
-  return (
-    <div className="flex">
-      <CreateMeetingModal projectID={7} onClose={() => {}} />
-    </div>
-  )
-}
